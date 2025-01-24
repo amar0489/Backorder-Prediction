fa6f5d4f6a9a8ecfb7e0a103cd2b99ef33460987
Amar Hulamani <158044853+amar0489@users.noreply.github.com>
2025-01-24 23:32:32 +0530

    Model Training

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from src.backorderproject.components.data_ingestion import DataIngestion
 from src.backorderproject.components.data_ingestion import DataIngestionConfig
 from src.backorderproject.components.data_transformation import DataTransformation
 from src.backorderproject.components.data_transformation import DataTransformationConfig
+from src.backorderproject.components.model_trainer import ModelTrainer
+from src.backorderproject.components.model_trainer import ModelTrainingConfig
+
 
 
 
@@ -18,3 +21,12 @@ if __name__== "__main__":
     train_arr_ovr, test_arr_ovr, preprocessor_path_ovr = data_transformation.initiate_data_transformation(train_data, test_data, sampling_strategy="oversample")
 
     train_arr_und, test_arr_und, preprocessor_path_und = data_transformation.initiate_data_transformation(train_data, test_data, sampling_strategy="undersample")
+
+    datasets= {
+        "none": (train_arr,test_arr),
+        "oversample": (train_arr_ovr,test_arr_ovr),
+        "undersample": (train_arr_und,test_arr_und)
+    }
+
+    modeltrainer= ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(datasets=datasets))
